118b969c5819bedff6c35ffaa17e9b602f90e67b
yastefan <stefanschmidt@posteo.org>
2022-05-01 18:02:26 +0200

    first implementation

---
+++ elm.js
@@ -0,0 +1,21 @@
+function init() {
+  script.log("elm module loaded");
+}
+
+function moduleParameterChanged(param) {
+}
+
+function moduleValueChanged(value) {
+}
+
+function changeIntensity(stage, intensity) {
+  local.send("/elm/stages/" + stage + "/live/intensity", intensity);
+}
+
+function changeSpeed(stage, speed) {
+  local.send("/elm/stages/" + stage + "/live/speed", speed);
+}
+
+function changeColor(stage, color) {
+  local.send("/elm/stages/" + stage + "/live/rgb", color[0], color[1], color[2]);
+}
